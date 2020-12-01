27c5daf1b2b0353edcbe6a37a906fb6c32e5f9cb
ILia Berestov <berestov_2000@bk.ru>
2020-12-01 22:18:15 +0300

    2

--- J3_2.java
+++ J3_2.java
@@ -1,14 +1,14 @@
 public class J3_2 {
     public static void main(String[] args) {
         int adding = 21000;
-        int customer_account = 1000;
+        int customerAccount = 1000;
         int bonus;
         if (adding >= 1100) {
             bonus = adding / 100;
         } else {
             bonus = 0;
         }
-        int conclusion = adding + bonus + customer_account ;
+        int conclusion = adding + bonus + customerAccount ;
         System.out.println(conclusion);
     }
 }
